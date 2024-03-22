c926f2a16eea6488d7ba71688299a60b7f0d2d30
Felix Nehren <felix@felixnehren.de>
2024-03-22 18:41:43 +0100

    add sudo to git pull

--- cronjob.py
+++ cronjob.py
@@ -5,7 +5,7 @@ import subprocess
 
 # Schedule the job to run every 60 minutes
 schedule.every(60).minutes.do(db_provider.refresh_db_values)
-schedule.every(2).minutes.do(subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout)
+schedule.every(2).minutes.do(subprocess.run(["sudo", "git", "pull"], check=True, stdout=subprocess.PIPE).stdout)
 
 while True:
     schedule.run_pending()

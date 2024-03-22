c8f9976c1b9f9118c4c7d0a38317834b0ff1be88
Felix Nehren <felix@felixnehren.de>
2024-03-22 17:04:24 +0100

    fix log path

--- helpers/last_run.py
+++ helpers/last_run.py
@@ -7,7 +7,7 @@ fmt = "%d.%m.%y %H:%M:%S"
 def get_last_run_time_stamp():
     # Try loading the datetime of the last run from the log file
     try:
-        with open("../logs/last_run.log", mode="r") as file:
+        with open("logs/last_run.log", mode="r") as file:
             lines = file.readlines()
             if lines:
                 return datetime.datetime.strptime(str(lines[-1].rsplit(" ", 1)[0]).strip(), fmt)
@@ -19,11 +19,11 @@ def get_last_run_time_stamp():
 
 
 def save_last_run_time_stamp(prefix='unknown'):
-    filename = "../logs/last_run.log"
+    filename = "logs/last_run.log"
 
     if not os.path.exists(filename):
         open(filename, "w+").close()
     # Update the script execution time and save it to the log file
-    with open("../logs/last_run.log", mode="a") as file:  # Use "a" for append mode
+    with open("logs/last_run.log", mode="a") as file:  # Use "a" for append mode
         current_timestamp = datetime.datetime.now().strftime(fmt) + " " + prefix
         file.write(current_timestamp + "\n")  # Write current timestamp to a new line

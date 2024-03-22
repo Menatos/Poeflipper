36082d28c2d3cd2506d3269dbb8cf4e3cb27a76d
Felix Nehren <felix@felixnehren.de>
2024-03-22 19:18:59 +0100

    fix cronjob.py

--- cronjob.py
+++ cronjob.py
@@ -1,11 +1,9 @@
 import time
 import schedule
 from database import db_provider
-import subprocess
 
 # Schedule the job to run every 60 minutes
 schedule.every(60).minutes.do(db_provider.refresh_db_values)
-schedule.every(2).minutes.do(subprocess.run(["sudo", "git", "pull"], check=True, stdout=subprocess.PIPE).stdout)
 
 while True:
     schedule.run_pending()
